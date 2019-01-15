3745b08aed57783cfc73ed79cac5c2427dceaab6
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-01-15 22:28:51 +0200

    Added a basic webapp functionality to list/add customers and view detailed history of a customer

---
+++ application/customers/models.py
@@ -0,0 +1,21 @@
+from application import db
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+
+    # FOREIGN KEY, organization.id
+    organization_id = db.Column(db.Integer, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    middle_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, first_name, middle_name, last_name, organization_id, balance):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.balance = balance
+        self.organization_id = organization_id
---
+++ application/customers/views.py
@@ -0,0 +1,35 @@
+from application import app, db
+from flask import redirect, render_template, request, url_for
+from application.customers.models import Customer
+
+@app.route("/customers/new")
+def customers_form():
+    return render_template("customers/new.html")
+
+@app.route("/customers", methods=["GET"])
+def customers_index():
+    return render_template("customers/list.html", customers=Customer.query.all())
+
+@app.route("/customers", methods=["POST"])
+def customers_create():
+    # Here we need to validate user input
+
+    new_customer = Customer (
+        request.form.get("first_name"),
+        request.form.get("middle_name"),
+        request.form.get("last_name"),
+        request.form.get("organization_id"),
+        request.form.get("balance")
+    )
+
+    db.session().add(new_customer)
+    db.session().commit()
+
+    return redirect(url_for("customers_index"))
+
+@app.route("/customers/<int:customer_id>", methods=["GET"])
+def customer_details(customer_id):
+    customer = Customer.query.get(customer_id)
+
+    return render_template("/customers/details.html", customer=customer)
+
