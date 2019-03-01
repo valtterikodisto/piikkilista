66c17984755bd5fb4ebaa18ccf8e0cbdf232639c
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-03-01 23:35:04 +0200

    Fixed registration form password validation

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -11,5 +11,5 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     username = StringField('Käyttäjänimi', [Length(min=2, max=144, message="Etunimen tulee olla vähintään 2 merkkiä ja korkeintaan 144 merkkiä")])
-    password = PasswordField('Salasana', [InputRequired(), EqualTo('confirm', message='Passwords must match'), Length(min=8, max=144 message="Salasanan tulee olla vähintään 8 merkkiä ja korkeintaan 144 merkkiä")])
+    password = PasswordField('Salasana', [InputRequired(), EqualTo('confirm', message='Passwords must match'), Length(min=8, max=144, message="Salasanan tulee olla vähintään 8 merkkiä ja korkeintaan 144 merkkiä")])
     confirm  = PasswordField('Salasana uudestaan')
